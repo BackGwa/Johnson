5d398ccf2fe9917b429771179073e8761c961d27
BackGwa <64456214+BackGwa@users.noreply.github.com>
2022-11-04 13:11:21 +0900

    Intelligent meal timetable bug fix
    [The following have been corrected]
    
    - Bug that was displayed abnormally when there was no meal
    
    - A bug that caused an error when the information in JSON was empty

--- johnson.py
+++ johnson.py
@@ -20,19 +20,62 @@ def meal_info(school_type, school_code, add_year, add_month, add_date, aleg):   
 
 def meal_zone(JSON, usetime, meal_value):                                                       # 급식 정보 반환
     mealzone = meal_value
+    data = json.loads(JSON.text)
+    result = ''
     
     if(usetime):
         hour = today.hour
-
-        if(hour < 7):
-            mealzone = 'breakfast'
-        elif(hour > 7 and hour < 14):
+        
+        isbf = True
+        islc = True
+        isdr = True
+        
+        if((data['menu'][0]['breakfast']) == []):
+            isbf = False
+        if((data['menu'][0]['lunch']) == []):
+            islc = False
+        if((data['menu'][0]['dinner']) == []):
+            isdr = False
+        
+        if(isbf and islc and isdr):
+            if(hour > 0 and hour < 8):
+                mealzone = 'breakfast'
+            elif(hour > 8 and hour < 14):
+                mealzone = 'lunch'
+            elif(hour > 14):
+                mealzone = 'dinner'
+                
+        elif(isbf and islc):
+            if(hour > 0 and hour < 8):
+                mealzone = 'breakfast'
+            elif(hour > 8):
+                mealzone = 'lunch'
+                
+        elif(islc and isdr):
+            if(hour > 0 and hour < 14):
+                mealzone = 'lunch'
+            elif(hour > 14):
+                mealzone = 'dinner'
+                
+        elif(islc):
             mealzone = 'lunch'
-        elif(hour > 14 and hour > 23):
-            mealzone = 'dinner'
             
-    data = json.loads(JSON.text)
-    return data['menu'][0][f'{mealzone}']   
+        else:
+            mealzone = 'None'
+            
+    if(usetime):
+        if(mealzone != 'None'):
+            result = data['menu'][0][f'{mealzone}']
+        else:
+            result = ['급식 정보가 존재하지 않습니다.']
+        
+    elif((data['menu'][0][meal_value]) != []):
+        result = data['menu'][0][f'{meal_value}']
+        
+    else:
+        result = ['급식 정보가 존재하지 않습니다.']
+            
+    return result
 
 def now(school_type, school_code, add_value, aleg, usetime, zonevalue = 'NoneValue'):
     
