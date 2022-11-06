e1ca0d2bec50eca4db31bd031d161a703f6959d8
BackGwa <kojk3254@naver.com>
2022-11-06 14:04:18 +0900

    Source code has been modified
    -> Add Defaults
    -> Change Function
    -> Code Optimization

--- johnson.py
+++ johnson.py
@@ -9,13 +9,8 @@ today = datetime.today()
 
 
 # [함수] : API로 급식 정보 JSON 반환하기
-def meal_info(school_type, school_code, add_year, add_month, add_date, aleg):
-    
-    year = today.year + add_year
-    month = today.month + add_month
-    day = today.day + add_date
+def meal_info(school_type, school_code, year, month, day, aleg):
     allergy = 'formed' if (aleg == True) else 'hidden'                                                       
-
     return f'https://schoolmenukr.ml/api/{school_type}/{school_code}?year={year}&month={month}&date={day}&allergy={allergy}'
 
 
@@ -44,17 +39,13 @@ def meal_zone(JSON, usetime, meal_value):
             mealzone = 'breakfast' if (hour > 0 and hour < 8) else 'lunch'  
         elif(islc and isdr):     
             mealzone = 'lunch' if (hour > 0 and hour < 14) else 'dinner'
-        elif(islc):
-            mealzone = 'lunch'
         else:
-            mealzone = 'None'
+            mealzone = 'lunch' if((islc) and ((not isbf) and (not isdr))) else 'None'
             
-    if(usetime):
         result = ([mealzone] + data['menu'][0][f'{mealzone}']) if (mealzone != 'None') else ['급식 정보가 존재하지 않습니다.']
-    elif((data['menu'][0][mealzone]) != []):
-        result = [mealzone] + data['menu'][0][f'{mealzone}']
+        
     else:
-        result = ['급식 정보가 존재하지 않습니다.']
+        result = [mealzone] + data['menu'][0][f'{mealzone}'] if((data['menu'][0][mealzone]) != []) else ['급식 정보가 존재하지 않습니다.']
             
     return result
 
@@ -65,9 +56,9 @@ def aleg_info(mealinfo):
 
 
 # [함수] : 급식 정보 가져오기
-def now(school_type, school_code, add_value, aleg, usetime, zonevalue = 'NoneValue'):
+def now(school_type, school_code, get_value = [today.year, today.month, today.day], aleg = False, usetime = True, zonevalue = 'NoneValue'):
     
-    API = meal_info(school_type, school_code, add_value[0], add_value[1], add_value[2], aleg)
+    API = meal_info(school_type, school_code, get_value[0], get_value[1], get_value[2], aleg)
     response = requests.get(API)                                                                
 
     result = meal_zone(response, usetime, zonevalue)
